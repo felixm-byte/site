6f5f00c49f2d5461d3dc80a405f0ab37432e018b
felixexists <72092800+felixm-byte@users.noreply.github.com>
2024-08-24 12:35:48 +0100

    Updated APEERS logo

--- components/sponsors.js
+++ components/sponsors.js
@@ -11,7 +11,7 @@ const data = {
       name: 'APEERS',
       link: 'https://apeers.org/',
       image:
-        'https://apeers.org/_next/image?url=%2Fimages%2Flogos%2Flogo.png&w=256&q=75'
+        'https://cloud-8exu5zwju-hack-club-bot.vercel.app/0image.png'
     },
     {
       name: 'Art of Problem Solving',
